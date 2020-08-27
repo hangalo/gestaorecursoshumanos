fbba35e1bfd332fe4d2b4d9de3c717178067ec65
Joaquim Hangalo <joajoshanggmail.com>
2020-08-27 16:55:05 +0200

    Envio do projecto para o repositorio

---
+++ src/main/java/rh/modelo/Funcionario.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rh.modelo;
+
+/**
+ *
+ * @author DGTALE
+ */
+public class Funcionario {
+    private String nome;
+    private String sobrenome;
+
+    public Funcionario() {
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+    
+    
+}
