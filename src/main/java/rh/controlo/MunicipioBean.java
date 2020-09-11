68bd9457d3e521ef1daadcfb03b95aa2013e442c
Joaquim Hangalo <joajoshanggmail.com>
2020-09-11 15:10:40 +0200

    Tabalho com data table

--- src/main/java/rh/controlo/MunicipioBean.java
+++ src/main/java/rh/controlo/MunicipioBean.java
@@ -23,26 +23,53 @@ import rh.modelo.Municipio;
 public class MunicipioBean {
 
     MunicipioDAO dao = new MunicipioDAO();
+    Municipio municipio = new Municipio();
     List<Municipio> municipios;
+    List<Municipio> municipiosProvincia;
+
+    String provincia;
 
     @PostConstruct
     public void inicizalizar() {
         municipios = new ArrayList<>();
     }
-    
-       public List<Municipio> getListaMunicipioos() {
+
+    public List<Municipio> getListaMunicipioos() {
         List<Municipio> lista = new ArrayList<>();
         lista = dao.listaMunicipios();
         return lista;
 
     }
-       
-       
-         public List<SelectItem> getSelectMunicipios() {
+
+    public List<Municipio> getListaMunicipiosDaProvincia() {
+        municipiosProvincia = new ArrayList<>();
+        municipiosProvincia = dao.listaMunicipiosByProvincia(provincia);
+        return municipiosProvincia;
+
+    }
+
+    public List<SelectItem> getSelectMunicipios() {
         List<SelectItem> lista = new ArrayList<>();
         for (Municipio m : dao.listaMunicipios()) {
             lista.add(new SelectItem(m, m.getNomeMunicipio()));
         }
         return lista;
     }
+
+    public String getProvincia() {
+        return provincia;
+    }
+
+    public void setProvincia(String provincia) {
+        this.provincia = provincia;
+    }
+
+    public Municipio getMunicipio() {
+        return municipio;
+    }
+
+    public void setMunicipio(Municipio municipio) {
+        this.municipio = municipio;
+    }
+
 }
